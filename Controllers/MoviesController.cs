9501d3a6ea5b981e7255db60a4d316ed435102fa
Kate Kalashnikova <kkalashnikova@kruxanalytics.com>
2021-07-21 15:00:49 -0600

    updates

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidley.Models;
+using Vidley.Models.ViewModels;
 
 namespace Vidley.Controllers
 {
@@ -13,8 +14,42 @@ namespace Vidley.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() {Name = "Shrek"};
+            var customers = new List<Customer>
+            {
+                new Customer() {Name = "Customer 1"},
+                new Customer() {Name = "Customer 2"}
+            };
 
-            return View(movie);
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
+
+            public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        //movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0} & sortBy={1}", pageIndex, sortBy));
         }
     }
 }
